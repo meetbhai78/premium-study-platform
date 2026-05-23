1543ec85d37e1fe999a219d7d658b1541eed1516
Study Developer <developer@study.com>
2026-05-23 17:37:05 +0530

    Fix password re-hashing hook bug on non-password profile updates

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -53,7 +53,7 @@ const userSchema = new mongoose.Schema({
 // Hash password before saving
 userSchema.pre('save', async function (next) {
   if (!this.isModified('passwordHash')) {
-    next();
+    return next();
   }
   const salt = await bcrypt.genSalt(10);
   this.passwordHash = await bcrypt.hash(this.passwordHash, salt);
